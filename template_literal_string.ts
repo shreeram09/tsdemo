381e1c0870b5b6062e933b54cca197f114065b61
shreeram09 <spaute@outlook.com>
2022-07-11 18:56:57 +0530

    string literal templating

---
+++ template_literal_string.ts
@@ -0,0 +1,8 @@
+namespace templatelitstr{
+
+    var fname:String =  "jessy";
+    var lname:string = 'blenn';
+    var age:number=34;
+    console.log(`hello ${fname} ${lname}, your age is ${age}`);
+    
+}
