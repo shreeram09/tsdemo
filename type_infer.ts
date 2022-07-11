efea2d402959d83b6e6d8492c7546d087d0ced37
shreeram09 <spaute@outlook.com>
2022-07-11 18:52:51 +0530

    data type, variable and function declarations

---
+++ type_infer.ts
@@ -0,0 +1,11 @@
+var number1:number = 10;
+var number2:number = 20;
+
+function add(n1:number,n2:number,n3:number){
+    var result = n1+n2+n3;
+    console.log(result);
+    
+    return result;
+}
+
+add(number1,number2,30);
