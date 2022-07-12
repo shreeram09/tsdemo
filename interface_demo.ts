84700ee8eba9cce7b8d8ad0236982d19f42d0749
shreeram09 <spaute@outlook.com>
2022-07-12 19:01:44 +0530

    abstract class and implementation

--- interface_demo.ts
+++ interface_demo.ts
@@ -1,5 +1,32 @@
 namespace interfacedemo
 {
+    abstract class AbsEmployee{
+        private salary:number;
+        constructor(salary:number){
+            this.salary=salary;
+        }
+        
+        getSalary(){
+            return this.salary;
+        }
+       abstract calculateTax():number;
+
+    }
+    class SalariedEmployee extends AbsEmployee{
+        private tax:number;
+        constructor(salary:number,tax?:number){
+            super(salary);
+            this.tax = tax || 0;
+        }
+        calculateTax():number{
+            if(super.getSalary()>500000.00)
+                return super.getSalary()*(this.tax/100);
+            return 0;
+        }
+        getSalary(): number {
+            return super.getSalary()-this.calculateTax();
+        }
+    }
     interface INotification
     {
         SMSNotify():void;
@@ -29,4 +56,10 @@ namespace interfacedemo
     obj.UploadVideo();
     obj.SMSNotify();
     obj.EmailNotify();
+
+    var emp:AbsEmployee = new SalariedEmployee(400000,10);
+    console.log(emp.calculateTax());
+    console.log(emp.getSalary());
+    
+    
 }
